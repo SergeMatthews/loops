c1a5b2018a2e73e0a3ed8a88a13123a56da1d3d5
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-15 11:20:42 +0600

    Add files via upload

---
+++ loop2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    int number = 0, digitsSum = 0;
+    cout << "Введите целое число:" << endl;
+    cin >> number;
+    if (number < 0)
+        number = -number;
+
+    while (number != 0)
+    {
+        digitsSum += number % 10;
+        number /= 10;
+    }
+    cout << "Сумма цифр: " << digitsSum << endl;
+
+    return 0;
+}
+
