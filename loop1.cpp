1b90b0933af96237771be05f76ad4bc415354255
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-15 11:20:23 +0600

    Add files via upload

---
+++ loop1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+
+    int user_number = 0, sum = 0;
+    do {
+        cout << "Введите целое число или число '0', чтобы закончить:" << endl;
+        cin >> user_number;
+        sum += user_number;
+    } while (user_number != 0);
+    cout << "Сумма: " << sum << endl;
+    
+    return 0;
+}
+
+
