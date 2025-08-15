2dbde09697bf4d676f536b303e643eb0e9e0f860
SergeMatthews <sergei_matveichuk94@mail.ru>
2025-08-15 11:21:01 +0600

    Add files via upload

---
+++ loop3.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    
+    int number = 0;
+    cout << "Введите целое число:" << endl;
+    cin >> number;
+    for (int i = 1; i <= 10; ++i)
+    {
+        cout << number << " x " << i << " = " << number * i << endl;
+    }
+
+    return 0;
+}
+
+
